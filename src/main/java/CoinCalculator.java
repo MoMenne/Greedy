4be9ba29eaf4e9b129ec82177a0d753320c58910
Brian Kane <bkane56@gmail.com>
2015-05-08 18:48:07 -0500

    this commit has working logic for greedy coin return

--- src/main/java/CoinCalculator.java
+++ src/main/java/CoinCalculator.java
@@ -1,17 +1,56 @@
-/**
- * Created by Brian on 5/6/2015.
- */
-public class CoinCalculator {
+
+import java.util.ArrayList;
+
+class CoinCalculator {
 
 
-    public String changeBack(String coinBack) {
-        Integer change = Integer.parseInt(coinBack.charAt(coinBack.length() - 1) + "");
-        if(change > 4) {
-            return "you get one nickel back";
-        } else if (change > 0) {
-            return "you get one penny back";
-        } else {
-            return "you get no coins back";
+    public String changeBack(String moneyAmmountIn) {
+
+        String digitsOnly = moneyAmmountIn.replaceAll("[^\\p{Digit}]+", "");
+        Integer change = Integer.parseInt(digitsOnly);
+
+        int totalCoins = 0;
+
+        if (change == 0)
+            return "you gave me no money, I'm not a fucking bank!";
+
+        System.out.println("Here is your change:\n");
+
+        int dollars = change / 100;
+        totalCoins += dollars;
+        change = change % 100;
+        if (dollars > 0) {
+            System.out.println(dollars + ((dollars > 1) ? " dollar coins.":" dollar coin."));
         }
+
+        int quarters = change / 25;
+        totalCoins += quarters;
+        change = change % 25;
+        if (dollars > 0) {
+            System.out.println(quarters + ((quarters > 1) ? " quarters." : " quarter."));
+        }
+
+        int dimes = change / 10;
+        totalCoins += dimes;
+        change = change % 10;
+        if (dollars > 0) {
+            System.out.println(dimes + ((dimes > 1) ? " dimes." : " dime."));
+        }
+
+        int nickles = change / 5;
+        totalCoins += nickles;
+        change = change % 5;
+        if (dollars > 0) {
+            System.out.println(nickles + ((nickles > 1) ? " nickles." : " nickle."));
+        }
+
+        int pennys = change;
+        totalCoins += pennys;
+        if (dollars > 0) {
+            System.out.println(pennys + ((pennys > 1) ? " pennys." : " penny."));
+        }
+        System.out.println("\n" + totalCoins + ((totalCoins > 1) ? " coins?" : " coin?") + ", I am allowed to accept tips.");
+
+        return Integer.toString(totalCoins);
     }
 }
--- src/test/java/CoinCalculatorTest.java
+++ src/test/java/CoinCalculatorTest.java
@@ -1,36 +1,24 @@
 
-/**
- * Created by Brian on 5/7/2015.
- */
+
 import org.testng.annotations.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class CoinCalculatorTest {
 
+//    @Test
+//    public void amountOfCoinsReturned() {
+//        CoinCalculator coinCalculator = new CoinCalculator();
+//
+//        String coinMessage = coinCalculator.changeBack("$18.69");
+//
+//        assertEquals("26", coinMessage);
+//    }
     @Test
     public void noChangeMeansNoCoins() {
         CoinCalculator coinCalculator = new CoinCalculator();
 
-        String coinMessage = coinCalculator.changeBack("$8.00");
+        String coinMessage = coinCalculator.changeBack("$0.00");
 
-        assertEquals("you get no coins back", coinMessage);
-    }
-
-    @Test
-    public void oneCentMeansOnePenny() {
-        CoinCalculator coinCalculator = new CoinCalculator();
-
-        String coinMessage = coinCalculator.changeBack("$8.01");
-
-        assertEquals("you get one penny back", coinMessage);
-    }
-
-    @Test
-    public void fiveCentsMeansOneNickel() {
-        CoinCalculator coinCalculator = new CoinCalculator();
-
-        String coinMessage = coinCalculator.changeBack("$8.05");
-
-        assertEquals("you get one nickel back", coinMessage);
+        assertEquals("you gave me no money, I'm not a fucking bank!", coinMessage);
     }
 }
